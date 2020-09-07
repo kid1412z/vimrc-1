3ff1cec7379d1e75bcf504370311d6998f6ec99e
Zuo Qy <zuoqy.gk@gmail.com>
2020-09-07 10:18:34 +0800

    Update download_plugs.sh

--- download_plugs.sh
+++ download_plugs.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 git clone https://github.com/arcticicestudio/nord-vim.git ./my_plugins/nord-vim
 git clone https://github.com/skywind3000/asyncrun.vim.git ./my_plugins/asyncrun.vim
+git clone https://github.com/weirongxu/plantuml-previewer.vim.git ./my_plugins/plantuml-previewer.vim
+git clone https://github.com/tyru/open-browser.vim.git ./my_plugins/open-browser.vim
+git clone https://github.com/aklt/plantuml-syntax.git ./my_plugins/plantuml-syntax.vim
